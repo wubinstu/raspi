27a302c3988a76752c9c0b0068227741e7ec9262
Einc <wubinstu@foxmail.com>
2022-04-30 22:41:49 +0800

    add some basic functions for monit and confOpt

--- src/types.c
+++ src/types.c
@@ -25,3 +25,29 @@ void catElems(Elem e[],int length)
 	for(int i = 0; i < length;i++)
 		printf("name = %s, value = %s\n",e[i].name,e[i].value);
 }
+
+
+void clearMonit(monit * mn)
+{
+	if(mn == NULL)
+		return;
+	mn->distance = 0;
+	mn->cpu_temper = 0;
+	mn->env_temper = 0;
+	mn->env_humidity = 0;
+}
+
+void default_confOpt(confOpt * co)
+{
+	if (co == NULL)
+		return;
+	co->serv_ip = inet_addr ("127.0.0.1");
+	co->serv_port = 9190;
+	co->interval = 10;
+	co->frectime = 42;
+	co->frecatps = 7;
+	co->checkMe = false;
+	bzero (co->CAfile,sizeof (co->CAfile));
+	bzero (co->UCert,sizeof (co->UCert));
+	bzero (co->UKey,sizeof (co->UKey));
+}
