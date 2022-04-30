708abe9af79e72173d9a7426be92d8ce290f470b
Einc <wubinstu@foxmail.com>
2022-04-30 22:30:29 +0800

    add struct confOpt

--- inc/types.h
+++ inc/types.h
@@ -5,7 +5,7 @@
 #include "head.h"
 
 /** For configuration file */
-typedef struct
+typedef struct element
 {
 	char name[10];
 	char value[90];
@@ -20,6 +20,19 @@ typedef struct monitoring
 	float env_humidity;
 }monit;
 
+typedef struct configuration_options
+{
+	unsigned long serv_ip;
+	unsigned short serv_port;
+	int interval;
+	int frectime;
+	int frecatps;
+	bool checkMe;
+	char CAfile[256];
+	char UCert[256];
+	char UKey[256];
+}confOpt;
+
 /** Fill data */
 extern void setElem(Elem * e,const char * name,const char * value);
 
@@ -31,5 +44,12 @@ extern void catElem(Elem e);
 
 /** Print elems */
 extern void catElems(Elem e[],int length);
+
+/** set struct value to zero */
+extern void clearMonit(monit * mn);
+
+/**
+ * set defalut value to configuration */
+extern void default_confOpt(confOpt * co);
 
 #endif
